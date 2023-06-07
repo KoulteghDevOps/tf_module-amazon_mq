1b31b928ccd6199048c55e4d599bea342c61f7f7
KoulteghDevOps <koultegh@outlook.com>
2023-06-07 16:45:26 +0100

    manifest updated

--- main.tf
+++ main.tf
@@ -52,8 +52,8 @@ resource "aws_instance" "rabbitmq" {
 }
 
 resource "aws_route53_record" "main" {
-  name_id = var.domain_id
-  mane    = "rabbitmq-${var.env}"
+  zone_id = var.domain_id
+  name    = "rabbitmq-${var.env}"
   type    = "A"
   ttl     = 30
   records = [aws_instance.rabbitmq.private_ip]
